e8828b7ae143e94fed53a081f5bbaf220280fd1f
Takaeroid <takenokoroid@gmail.com>
2020-11-12 15:26:13 +0900

    ローカルでのテスト完了

--- firebase/functions/sampleIndex.js
+++ firebase/functions/sampleIndex.js
@@ -8,72 +8,50 @@ admin.initializeApp(functions.config().firebase);
 
 const db = admin.firestore();
 
-
 const _setGeoWeather = (data) => {
-  const docRef = db.collection("weather").doc("one");
-  docRef.set({
-    date: data.daily[0].dt,
-    weatherIcon: data.daily[0].weather[0].icon,
+  data.daily.forEach((elem, index) => {
+    const docRef = db.collection("weather").doc(`${index}`);
+    docRef.set({
+      date: elem.dt,
+      weatherIcon: elem.weather[0].icon,
+    });
   });
 };
 
-const _fetchGeoWeather = () => {
+const _fetchGeoWeather = async () => {
   const lat = "35.685973";
   const lon = "139.756597";
   const exclude = "current,minutely,hourly,alerts";
   const endpoint = "https://api.openweathermap.org/data/2.5/onecall";
-  const appid = "YOUR_API_KEY";
-  const url = `${endpoint}?lat=${lat}&lon=${lon}&exclude${exclude}&appid=${appid}`;
-  let weatherJSON="ok"
-  fetch(url, {
+  const appid = "YOUR API KEY";
+  const url = `${endpoint}?lat=${lat}&lon=${lon}&exclude=${exclude}&appid=${appid}`;
+  let weatherJSON = "ok";
+  const response = await fetch(url, {
     method: "GET",
   })
     .then((response) => response.json())
     .then((data) => {
-      weatherJSON=data
-      return data;
+      weatherJSON = data;
+      return weatherJSON;
     })
     .catch((err) => {
       console.log(err);
     });
-
-    return weatherJSON
+  return response;
 };
 
+exports.date = functions.https.onRequest((req, res) => {
+  const val = _fetchGeoWeather().then((response) => {
+    _setGeoWeather(response);
+    return res.send("send");
+  });
+});
+
+/*
 exports.fetchGeoWeather = functions.pubsub
   .schedule("0 6 * * *")
   .timeZone("Asia/Tokyo")
   .onRun((content) => {
     return _fetchGeoWeather;
   });
-
-exports.date = functions.https.onRequest((req, res) => {
-  const val =_fetchGeoWeather();
-  res.send("send"+val)
-});
-
-exports.setGeoWeather = functions.https.onRequest((req, res) => {
-  const sampleData={daily:[
-    {
-      dt:1900,
-      weather:[{icon : "10d"}
-      ],
-    }
-    ]
-  }
-  _setGeoWeather(sampleData);
-  res.send("send")
-});
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  response.send("Hello from Firebase!");
-});
-
-
-exports.addMessage = functions.https.onRequest((req, res) => {
-  // Grab the text parameter.
-  
-  // Push the new message into Cloud Firestore using the Firebase Admin SDK.
-  const writeResult = admin.firestore().collection('messages').add({original: "test"});
-  // Send back a message that we've succesfully written the message
-  res.json({result: `Message with ID: ${writeResult.id} added.`});
-});
+*/
